53f5c7714b6f872fff26272a187862fbd0cf29ce
ramik1990 <ramikshudabaev@gmail.com>
2024-03-31 13:34:22 +0500

    Init: add VueJS in project

---
+++ resources/js/Components/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>New Mosheyah on VueJS</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+h1 {
+    color: red;
+    font-size: 30px;
+    font-weight: bolder;
+    text-align: center;
+}
+</style>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
-import './bootstrap';
+import '../css/app.css'
+
+import { createApp } from 'vue'
+import App from './Components/App.vue'
+
+createApp(App).mount('#app')
