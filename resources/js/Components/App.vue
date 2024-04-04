ba1c06bbb89d48845f12e64ca0ca9c6847c80f2f
ramik1990 <ramikshudabaev@gmail.com>
2024-04-04 23:42:07 +0500

    Feat: Add ReviewsMainPage component

--- resources/js/Components/App.vue
+++ resources/js/Components/App.vue
@@ -5,6 +5,7 @@
         <news-main-page></news-main-page>
         <about-main-page></about-main-page>
         <contacts-main-page></contacts-main-page>
+        <reviews-main-page></reviews-main-page>
     </main>
 </template>
 
@@ -14,6 +15,7 @@ import HeaderMainPage from './mainApp/HeaderMainPage.vue'
 import NewsMainPage from './mainApp/NewsMainPage.vue'
 import AboutMainPage from './mainApp/AboutMainPage.vue'
 import ContactsMainPage from './mainApp/ContactsMainPage.vue'
+import ReviewsMainPage from './mainApp/ReviewsMainPage.vue'
 
 export default {
     components: {
@@ -21,7 +23,8 @@ export default {
         'header-main-page': HeaderMainPage,
         'news-main-page': NewsMainPage,
         'about-main-page': AboutMainPage,
-        'contacts-main-page': ContactsMainPage
+        'contacts-main-page': ContactsMainPage,
+        'reviews-main-page': ReviewsMainPage
     }
 }
 </script>
---
+++ resources/js/Components/mainApp/ReviewsMainPage.vue
@@ -0,0 +1,13 @@
+<template>
+    <h3>ReviewsMainPage</h3>
+</template>
+
+<script>
+export default {
+    name: "ReviewsMainPage"
+}
+</script>
+
+<style scoped>
+
+</style>
