8d11a0f31173f57c7a17997ac5604d1a9cbcb4e4
ramik1990 <ramikshudabaev@gmail.com>
2024-04-06 15:56:07 +0500

    Feat: complete ReviewsMainPage component

---
+++ resources/js/Components/mainApp/ReviewsBlockItem.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="reviews-block">
+        <img src="../../../images/logos/male_avatar.png" alt="avatar" class="avatar">
+        <p class="reviews-block-name">Евгений</p>
+        <p class="reviews-block-description">
+            Отличный магазин! Профессиональные консультации, широкий выбор систем пожаротушения, быстрая доставка. Надежные партнеры в обеспечении безопасности вашего бизнеса. Рекомендую!
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ReviewsBlockItem"
+}
+</script>
+
+<style scoped>
+.reviews-block {
+    width: 100%;
+    max-width: 453.34px;
+    min-width: 200px;
+    margin-top: 100px;
+    height: 300px;
+    background-color: #fff;
+    display: flex;
+    flex-direction: column;
+    justify-content: end;
+    border: 1px solid rgba(0, 0, 0, .01);
+    box-shadow: 0 2px 4px #ee6a59cc;
+    position: relative;
+    padding: 20px 0;
+}
+.avatar {
+    position: absolute;
+    left: calc(50% - 100px);
+    top: -25%;
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+}
+.reviews-block-name {
+    position: absolute;
+    top: calc(25% + 45px);
+    width: 100%;
+    text-transform: uppercase;
+    color: #3a3f58;
+    text-align: center;
+    font-family: Inter;
+    font-size: 30px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+}
+.reviews-block-description {
+    width: calc(100% - 40px);
+    height: 150px;
+    color: #3a3f58;
+    text-align: justify;
+    font-family: Inter;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 700;
+    overflow: hidden;
+    line-height: normal;
+    padding: 0 20px;
+}
+</style>
--- resources/js/Components/mainApp/ReviewsMainPage.vue
+++ resources/js/Components/mainApp/ReviewsMainPage.vue
@@ -1,13 +1,79 @@
 <template>
-    <h3>ReviewsMainPage</h3>
+    <section class="reviews">
+        <h2 class="reviews-title">отзывы</h2>
+        <div class="reviews-body">
+            <reviews-block-item></reviews-block-item>
+            <reviews-block-item></reviews-block-item>
+            <reviews-block-item></reviews-block-item>
+        </div>
+        <a href='#' class="add-review-button">добавить отзыв</a>
+    </section>
 </template>
 
 <script>
+import ReviewsBlockItem from './ReviewsBlockItem.vue'
+
 export default {
+    components: {
+        'reviews-block-item':ReviewsBlockItem
+    },
     name: "ReviewsMainPage"
 }
 </script>
 
 <style scoped>
-
+.reviews {
+    width: calc(100% - 40px);
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    background: #fff;
+    gap: 20px;
+}
+.reviews .reviews-title {
+    width: 100%;
+    text-align: center;
+    position: relative;
+    color: #3a3f58;
+    font-family: Inter;
+    font-size: 30px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    text-transform: uppercase;
+}
+.reviews .reviews-title::after{
+    position: absolute;
+    content: '';
+    bottom: 0;
+    left: calc(50% - 40px);
+    width: 80px;
+    height: 3px;
+    border: none;
+    background: #ee6a59;
+}
+.reviews-body {
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(433px, 1fr));
+    gap: 20px;
+    justify-items: center;
+}
+.add-review-button {
+    margin: auto auto 5px;
+    width: 200px;
+    height: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-transform: uppercase;
+    text-decoration: none;
+    background-color: #ee6a59;
+    color: #fff;
+}
+.add-review-button:hover {
+    transform: scale(1.02);
+}
 </style>
