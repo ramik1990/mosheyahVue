09ace7a8d3c1702a9c227f6787a89e33329f2cae
ramik1990 <ramikshudabaev@gmail.com>
2024-04-09 00:03:24 +0500

    Feat: complete CallWindow component

--- resources/js/Components/mainApp/CallWindow.vue
+++ resources/js/Components/mainApp/CallWindow.vue
@@ -2,6 +2,15 @@
     <div id="call-window">
         <div class="close-button">
             <span @click="closeWindow">&#10060;</span>
+            <div class="call-window-body">
+                <h3>заказать звонок</h3>
+                <form action="" method="post" id="contact-form">
+                    <input type="text" name="name" id="name" v-model="name" placeholder="Введите имя" required>
+                    <input type="text" name="phone" id="phone" v-model="phone" placeholder="Ваш номер телефона" required pattern="[0-9]{10}" title="Формат: 77772223344 (10 цифр номера телефона без '8' и '+7')">
+                    <textarea name="message" id="message" v-model="message" cols="35" rows="10" placeholder="Введите ваше сообщение"></textarea>
+                    <button type="submit" class="send-button" @click.prevent="sendForm">Отправить</button>
+                </form>
+            </div>
         </div>
     </div>
 </template>
@@ -10,9 +19,19 @@
 export default {
     name: "CallWindow",
     props:['openCallWindow'],
+    data(){
+        return {
+            name: '',
+            phone: '',
+            message: ''
+        }
+    },
     methods: {
         closeWindow(){
             this.openCallWindow(false);
+        },
+        sendForm() {
+            alert('name: '+this.name+'\nphone: '+this.phone+'\nmessage: '+this.message);
         }
     }
 }
@@ -27,7 +46,7 @@ export default {
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-    border: 1px solid rgba(0, 0, 0, .01);
+    border: 1px solid #ee6a59;
     box-shadow: 0 2px 4px #ee6a59cc;
     display: flex;
     animation: openWindow .3s ease-in-out;
@@ -46,6 +65,54 @@ export default {
     transform: scale(1.02);
     background-color: rgba(0, 0, 0, 0.068);
     border-radius: 50%;
+}
+.call-window-body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    width: 100%;
+}
+.call-window-body h3 {
+    width: 100%;
+    text-align: center;
+    text-transform: uppercase;
+    margin-bottom: 20px;
+    font-weight: 700;
+    font-family: Inter;
+}
+#contact-form {
+    width: 100%;    
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 20px;
+}
+#contact-form input {
+    width: 95%;
+    font-size: 16px;
+    font-family: Inter;
+}
+.send-button {
+    width: 50%;
+    height: 30px;
+    font-family: Inter;
+    font-size: 14px;
+    text-transform: uppercase;
+    font-weight: 700;
+    background-color: #ee6a59;
+    border: none;
+    color: #fff;
+    cursor: pointer;
+}
+.send-button:hover {
+    transition: all ease .3s;
+    background-color: #fff;
+    color: #ee6a59;
+    border: 1px solid #ee6a59;
+    box-shadow: 0 2px 4px #ee6a59cc;
+    transform: scale(1.05);
 }
 
 @keyframes openWindow {
--- resources/js/app.js
+++ resources/js/app.js
@@ -5,4 +5,4 @@ import App from './Components/App.vue'
 import AdminApp from './Components/AdminApp.vue'
 
 createApp(App).mount('#app')                // Основное приложение VueJS
-createApp(AdminApp).mount('#adminapp')      // Административная часть приложение VueJS
+//createApp(AdminApp).mount('#adminapp')      // Административная часть приложение VueJS
